5891d1b5da697822ed5714375dce910d9b7e06e4
princebose <boseprince11@gmail.com>
2020-07-02 14:34:53 -0400

    commit #3

--- server.js
+++ server.js
@@ -1,13 +1,64 @@
 var express = require('express')
-
 var app = express()
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser')
 
-// app.get('/',function(req,res){
-//     res.send("Hello World from server.js")
+var db = mongojs('contactlist',['contactlist'])
+
+// app.get('/', function(req, res){
+//     res.send("Hello world from server.js")
 // })
 
-app.use(express.static(__dirname+'/public/'))//__dirname automatically gets current dir
+//html, css, js
+app.use(express.static(__dirname + '/public'))
 
-app.listen(3000)
-console.log("Server up and running on 3000")
+app.use(bodyParser.json())
 
+
+app.get('/contactlist', function(req, res){
+    console.log("Express received a GET request")
+
+    db.contactlist.find(function(err, docs){
+        console.log(docs)
+        res.json(docs)
+    })
+});
+
+
+app.post('/contactlist', function(req, res){
+    console.log(req.body)
+    db.contactlist.insert(req.body, function(err, doc){
+        res.json(doc)
+    })
+})
+
+
+app.delete('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.remove({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+})
+
+app.get('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.findOne({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+});
+
+app.put('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(req.body.name)
+    db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
+        update: {$set:{name:req.body.name, email:req.body.email, number:req.body.number}},
+        new:true}, function(err, doc){
+            res.json(doc);
+            console.log(err)
+        })   
+})
+
+app.listen(3000);
+console.log("Server running on port 3000")
